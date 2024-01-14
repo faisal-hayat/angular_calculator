1a065e5b98ed5de8c4b2e9dfb184bdd8bdcf3a69
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-01-14 17:52:06 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# K-Means
+
+--- ---
+
+## Work
+
+1. Usmnab Bhai Referred Code Example
+
+--- ---
---
+++ instructions.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# Run the following instructions to download thereequired libraries individually
+
+# to create virtual env
+python -m venv /path/to/new/virtual/environment
+# To activate the venv
+env\Scripts\activate
