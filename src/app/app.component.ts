7f143cd830cd1103d33cdb9fbbbcde1c2b776a9d
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-30 19:25:55 +0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,8 @@ export class AppComponent {
   result: number = 0;
   currentFunction: string = 'No Function';
   calNumber: string = 'noValue';
+  firstNumber: number = 0;
+  secondNumber: number = 0;
   //#endregion
 
   //#region constructor
@@ -30,6 +32,10 @@ export class AppComponent {
   onClickFunc(value: string, type?: string): void{
     if(type === 'number'){
       this.onNumberClick(value);
+    }
+    else if(type === 'function'){
+      // on function clicked
+      this.onFunctionClick(value);
     }
   }
   //#endregion
@@ -44,4 +50,45 @@ export class AppComponent {
     this.result = parseFloat(this.calNumber);
   }
   //#endregion
+  
+  //#region  when a function is clicked
+  onFunctionClick(value: string) {
+    if(this.currentFunction === "No Function"){
+      this.firstNumber = parseFloat(this.calNumber);
+      this.calNumber = '0';
+      this.currentFunction = value;
+    }else if(this.currentFunction !== "No Function"){
+      this.secondNumber = parseFloat(this.calNumber);
+      // perform calculation is here
+      this.currentFunction = value;
+      this.calculateValue(value);
+    }
+    // throw new Error('Method not implemented.');
+  }
+  //#endregion
+
+  //#region perform the calcualtion
+  calculateValue(value: string) {
+    if(this.currentFunction === "*"){
+      this.result = this.firstNumber * this.secondNumber;
+      this.calNumber = this.result.toString();
+      this.firstNumber = this.result;
+      this.secondNumber = 0;
+    }else if(this.currentFunction === "*"){
+      this.result = this.firstNumber + this.secondNumber;
+    }else if(this.currentFunction === "-"){
+      this.result = this.firstNumber - this.secondNumber;
+    }else if(this.currentFunction === "/"){
+      this.result = this.firstNumber / this.secondNumber;
+    }else if(this.currentFunction === "%"){
+      this.result = this.firstNumber % this.secondNumber;
+    }else if(this.currentFunction === "c"){
+      this.result = 0;
+      this.firstNumber = 0;
+      this.secondNumber = 0;
+      this.calNumber = 'noValue';
+    }
+  }
+  //#endregion
+
 }
